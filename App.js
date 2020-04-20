bf1cc7006b800ab7891bb098b07071aff671ab4c
GuillermoCAB <guiga.benitez@gmail.com>
2020-04-20 18:22:25 -0300

    iniciei o desenvolvimento do Header

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import { CurrencyProvider } from './src/providers/CurrencyProvider'
 import Header from './src/components/Header'
 import Header2 from './src/components/Header2'
 
@@ -11,12 +12,14 @@ const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
+    <CurrencyProvider>
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Header" component={Header} />
-        <Tab.Screen name="Header2" component={Header} />
+        <Tab.Screen name="Header2" component={Header2} />
       </Tab.Navigator>
     </NavigationContainer>
+    </CurrencyProvider>
   )
 } 
 
